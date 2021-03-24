016334704ea0ad9f57d7065dd8767d38800f5f48
MarioMarioPichoneau <20197959@itla.edu.do>
2021-03-24 19:23:49 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 6 crear un programa que pida al usuario su cedula y su contraseña (ambos numero), y no le permita seguir 
+            // hasta que introduzca como cedula "00113764530" y como contraseña "1823"
+
+
+            var cedula = "00113764530";
+            var contr = "1823";
+
+            Console.WriteLine(" ingrese su cedula para verificar usuario");
+            string  ced = (Console.ReadLine());
+
+            Console.WriteLine(" ingrese la contraseña");
+            string con = (Console.ReadLine());
+
+            while (ced != cedula && con != contr)
+            {
+                Console.WriteLine($" usuario incorrecto \n");
+                
+
+                Console.WriteLine(" ingrese su cedula para verificar nuevamente ");
+                ced = (Console.ReadLine());
+
+                Console.WriteLine(" ingrese su cedula para verificar");
+                con = (Console.ReadLine());
+
+            }
+
+            Console.WriteLine($" cedula y contraseña  correcta puede serguir adelante");
+
+            Console.Read();
+        }
+    }
+}
